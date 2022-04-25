1ddee68e25f0a6bd673f9c48698272a8a111c26e
jakechaley <jakechaley@gmail.com>
2022-04-25 13:50:27 -0700

    add toggle between tap list and new tap form

--- src/Components/TapControl.js
+++ src/Components/TapControl.js
@@ -6,16 +6,35 @@ class TapControl extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false 
+    };
   }
 
-  render() {
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if(this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTapForm/>
+      buttonText= "Return To Tap List";
+    } else {
+      currentlyVisibleState = <TapList/>
+      buttonText= "Add Tap"
+    }
+
     return (
       <React.Fragment>
-
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
+  
 }
 
 export default TapControl;
